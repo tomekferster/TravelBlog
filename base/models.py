02a90c8428ed3f5f63092d7f0e4c606de657fb6d
Tomek <tomasz.ferster@gmail.com>
2023-09-27 14:28:12 -0400

    profile additions

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import Profile
 import uuid
 # Create your models here.
 
@@ -6,7 +7,7 @@ import uuid
 class TravelPost(models.Model):
     # id = models.UUIDField(default=uuid.uuid4, unique=True,
     #                       primary_key=True, editable=False)   # I might want to use this one for its advantages
-    # author = 
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
     # topic =
     # commentators =
     name = models.CharField(max_length=150)
@@ -15,7 +16,7 @@ class TravelPost(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     image = models.ImageField(default='default_post_img.jpg', null=True, blank=True)
-    tags = models.ManyToManyField('Tag', null=True, blank=True)         # used quotes on Tag, because the Tag class is below the TravelPost class
+    tags = models.ManyToManyField('Tag', blank=True)         # used quotes on Tag, because the Tag class is below the TravelPost class
    
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -8,6 +8,9 @@ class Profile(models.Model):
     email = models.EmailField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     image = models.ImageField(default='profile_pictures/default_profile_img.jpg', null=True, blank=True, upload_to='profile_pictures/')
-
+    created = models.DateTimeField(auto_now_add=True)
+    social_instagram = models.CharField(max_length=200, null=True, blank=True)
     
+    def __str__(self):
+        return self.user.username
 
